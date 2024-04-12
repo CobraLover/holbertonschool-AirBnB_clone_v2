03b4db303e6c87f6b6fe93717db817a8d485d6f6
CobraLover <6716@holbertonstudents.com>
2024-04-12 15:29:11 +0200

    Task.0-10

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -1,31 +1,42 @@
 #!/usr/bin/python3
 """Starts a Flask web application"""
 
+from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from flask import Flask
-from flask import render_template
+
 app = Flask(__name__)
+
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Returns a rendered html template,
-    using the web_static files
-    """
-    states = storage.all('State').values()
+    """ display a HTML page like 8-index.html, done during the 0x01. 
+    AirBnB clone - Web static project Copy files 3-footer.css, 
+    3-header.css, 4-common.css, 6-filters.css and 8-places.css 
+    from web_static/styles/ to the folder web_flask/static/styles 
+    Copy all files from web_static/images/ to the folder web_flask/static/images 
+    Update .popover class in 6-filters.css to enable scrolling in the popover 
+    and set max height to 300 pixels. Update 8-places.css to always have the price 
+    by night on the top right of each place element, and the name correctly aligned 
+    and visible (i.e. screenshots below) Use 8-index.html content as source code 
+    for the template 100-hbnb.html: Replace the content of the H4 tag under each filter 
+    title (H3 States and H3 Amenities) by &nbsp; Make sure all HTML tags from objects 
+    are correctly used (example: <BR /> must generate a new line) State, City, Amenity 
+    and Place objects must be loaded from DBStorage and sorted by name (A->Z) """
     cities = storage.all('City').values()
-    amenities = storage.all('Amenity').values()
+    states = storage.all('State').values()
     places = storage.all('Place').values()
+    amenities = storage.all('Amenity').values()
     return render_template('100-hbnb.html', **locals())
 
 
 @app.teardown_appcontext
 def teardown(self):
-    """Removes the current SQLAlchemy Session"""
+    """ Removes the current SQLAlchemy Session """
     storage.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0')
