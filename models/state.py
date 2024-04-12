dd6134e03a3844ea4cacea5b9bba9cfa092321b5
CobraLover <6716@holbertonstudents.com>
2024-04-12 18:08:45 +0200

    Task.0-10

--- models/state.py
+++ models/state.py
@@ -1,32 +1,27 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
-from sqlalchemy.orm import relationship, backref
+""" State Module for HBNB project """
 import models
-from os import environ
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
+    """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
 
-    if environ.get('HBNB_TYPE_STORAGE') == "db":
-        cities = relationship("City", 
-                backref="state", cascade="all, delete, delete-orphan")
-    else:
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """ Returns the list of City instances with
-            state_id == current State.id """
-            all_cities = models.storage.all(City)
-            state_cities = []
-            for city_ins in all_cities.values():
-                if city_ins.state_id == self.id:
-                    state_cities.append(city_ins)
-            return state_cities
+            """Getter attribute cities that returns the list of City"""
+            from models import storage
+            from models.city import City
+            my_list = []
+            all_city = storage.all(City)
+            for city in all_city.values():
+                if city.state_id == self.id:
+                    my_list.append(city)
+            return my_list
