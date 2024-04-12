a31a3370d7abeb24c79fbe6f2aca9eb12b42663a
CobraLover <6716@holbertonstudents.com>
2024-04-12 16:32:51 +0200

    Task.0-10

--- models/state.py
+++ models/state.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from os import getenv
+"""Defines the state class."""
+
+import models
 from sqlalchemy import String, DateTime, Column, ForeignKey
 from sqlalchemy.orm import relationship
-import models
 from models.base_model import BaseModel, Base
 from models.city import City
+from os import getenv
 
 
 class State(BaseModel, Base):
-    """This is the class for State
+    """Represents a state for a MySQL database.
+
+
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
+
+
     Attributes:
-        name: input name
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
     """
+
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('City', backref='state',
-                              cascade='all, delete-orphan')
+        cities = relationship('City', backref='state', cascade='all, delete-orphan')
     else:
         @property
         def cities(self):
