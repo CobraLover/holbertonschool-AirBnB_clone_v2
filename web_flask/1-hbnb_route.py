17fce6146d384bfaf5cf982ca7631a60e51522a2
CobraLover <6716@holbertonstudents.com>
2024-04-12 10:00:50 +0200

    Task.0-10

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,20 +4,25 @@ Script that starts a Flask web application:
 listening on 0.0.0.0, port 5000
 With two Routes
 """
-from flask import Flask, escape, request
+
+from flask import Flask
+
 app = Flask(__name__)
 
 
+
 @app.route('/', strict_slashes=False)
-def hello():
-    """ First Route that display Hello HBNB"""
+def hello_hbnb():
+    """ display Hello HBNB! """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hello_hbnb():
-    """ Second Route that display HBNB"""
+def hbnb():
+    """ display HBNB """
     return "HBNB"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
