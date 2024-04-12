1ecf7565df094d44d43bce03a00c0347396da0e3
CobraLover <6716@holbertonstudents.com>
2024-04-12 17:46:22 +0200

    Task.0-10

--- models/state.py
+++ models/state.py
@@ -1,27 +1,31 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
+"""This is the state class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-import models
+from os import getenv
 from models.city import City
-import os
+import models
 
 
 class State(BaseModel, Base):
-    """ State class """
-    __tablename__ = "states"
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="delete", backref="state")
+    cities = relationship(
+        'City',
+        backref='cities',
+        cascade='all, delete',
+        order_by="City.name"
+    )
 
-    if os.getenv('HBNB_TYPE_STORAGE') != "db":
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """ cities getter attribute """
-            cit_lis = []
-            all_cit = models.storage.all(City)
-            for city in all_cit.values():  # change .items() to values() as it
-                # returns an obj that contains values of a dictionary as a list
-                if city.state_id == self.id:
-                    cit_lis.append(city)
-            return cit_lis
+            obj = models.storage.all(City)
+            ls = [v for k, v in obj.items() if v.state_id == self.id]
+            sorted(ls, key=lambda city: city.name)
+            return ls
