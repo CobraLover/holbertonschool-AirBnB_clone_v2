66a15822d43d33a9cb0d88063aead6bcd9ab9e96
CobraLover <6716@holbertonstudents.com>
2024-04-12 10:52:52 +0200

    Task.0-10

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -4,39 +4,50 @@ Script that starts a Flask web application:
 listening on 0.0.0.0, port 5000
 with Fifth routes
 """
-from flask import Flask, escape, request
+
+from flask import Flask, request
+
 app = Flask(__name__)
 
 
+
 @app.route('/', strict_slashes=False)
-def hello():
-    """ First Route that display Hello HBNB"""
+def hello_hbnb():
+    """ display Hello HBNB! """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hello_hbnb():
-    """ Second Route that display HBNB"""
+def hbnb():
+    """ display HBNB """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def hello_text(text):
-    """ Third Route that display C and text"""
-    return "C {}".format(text.replace("_", " "))
+def c_text(text):
+    """ display “C ”, followed by the value of the <text> 
+
+    replace underscore _ symbols with a space """
+    formatted_text = text.replace('_', ' ')
+    return "C {}".format(formatted_text)
 
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def hello_python(text):
-    """ Fourth Route that display C and text """
-    return "Python {}".format(text.replace("_", " "))
+def python_text(text):
+    """ display “Python ”, followed by the value of the <text> 
+
+    replace underscore _ symbols with a space """
+    formatted_text = text.replace('_', ' ')
+    return "Python {}".format(formatted_text)
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def hello_number(n):
-    """ Fifth Route that display C and text """
-    return "%d is a number" % n
+def number(n):
+    """ display “n is a number” only if n is an integer """
+    return "{:d} is a number".format(n)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
