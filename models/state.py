9a1654a608cfc56a13e4bd6c37d5f6af2fb1cef2
CobraLover <6716@holbertonstudents.com>
2024-04-12 17:11:26 +0200

    Task.0-10

--- models/state.py
+++ models/state.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Defines the state class."""
-
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
@@ -17,18 +16,19 @@ store = os.getenv('HBNB_TYPE_STORAGE')
 
 class State(BaseModel, Base):
     """This is the class for State
-    Attributes: name: input name
+    Attributes: 
+        name: input name
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if store == 'db':
         cities = relationship('City', backref='state', cascade='all, 
                 delete, delete-orphan')
-    else:
+
         @property
         def cities(self):
             """Getter attribute in case of file storage"""
+            from models import storage
             list = []
             for city in storage.all(City).values():
                 if city.state_id == self.id:
