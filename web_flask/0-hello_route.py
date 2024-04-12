d75c098d880ce9d3774ee7ea4a078337e29c8002
CobraLover <6716@holbertonstudents.com>
2024-04-12 09:29:40 +0200

    Task.0-10

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,14 +3,16 @@
 Script that starts a Flask web application:
 listening on 0.0.0.0, port 5000
 """
+
 from flask import Flask
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """ First Route that display Hello HBNB """
-    return 'Hello HBNB!'
+    """ First Route that display Hello HBNB! """
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     """ Main Function """
