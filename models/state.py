e908f8cc2921e6cb852e1b8b4a0e1b1c9e9a8109
CobraLover <6716@holbertonstudents.com>
2024-04-12 17:24:01 +0200

    Task.0-10

--- models/state.py
+++ models/state.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python3
-""" holds class State"""
-import models
+"""
+    Implementation of the State class
+"""
 from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.city import City
+from os import getenv
+import models
+
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
-    """Representation of state """
-    if models.storage_t == "db":
-        __tablename__ = 'states'
+    '''
+        Implementation for the State.
+    '''
+    __tablename__ = 'states'
+    if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship("City", cascade="all, delete", backref="states")
+        cities = relationship("City", backref="state",
+                              cascade="all, delete-orphan")
     else:
         name = ""
 
-    def __init__(self, *args, **kwargs):
-        """initializes state"""
-        super().__init__(*args, **kwargs)
-
-    if models.storage_t != "db":
+    if storage_type != 'db':
         @property
         def cities(self):
-            """getter for list of city instances related to the state"""
-            city_list = []
+            """
+            get list of City instances with state_id
+            equals to the current State.id
+            """
+            list_cities = []
             all_cities = models.storage.all(City)
-            for city in all_cities.values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-            return city_list
+            for key, city_obj in all_cities.items():
+                if city_obj.state_id == self.id:
+                    list_cities.append(city_obj)
+            return list_cities
