c607591e205d36714b66f72d4b846013854466ad
CobraLover <6716@holbertonstudents.com>
2024-04-12 07:40:56 +0200

    Task.0-10

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,9 +8,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """ First Route that display Hello HBNB"""
-    return "Hello HBNB!"
+def hello_hbnb():
+    """ First Route that display Hello HBNB """
+    return 'Hello HBNB!'
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
