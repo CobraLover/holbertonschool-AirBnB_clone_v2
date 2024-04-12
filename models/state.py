a666087c68e942f3152dc965c403b9e803d0b54f
CobraLover <6716@holbertonstudents.com>
2024-04-12 17:33:14 +0200

    Task.0-10

--- models/state.py
+++ models/state.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python3
-"""Defines the State class."""
+#!/usr/bin/python3
+""" State Module for HBNB project """
+
 import models
-from models.base_model import Base, BaseModel
-from models.city import City
 from os import getenv
-from sqlalchemy import Column, String
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
@@ -18,18 +23,26 @@ class State(BaseModel, Base):
         name (sqlalchemy String): The name of the State.
         cities (sqlalchemy relationship): The State-City relationship.
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City",  backref="state", cascade="delete")
-
+    __tablename__ = 'states'
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade="all,delete", backref="state")
     else:
+        name = ""
+        # DONE: for FileStorage: getter attribute cities that
+        # returns the list of City instances with state_id equals
+        # to the current State.id => It will be the FileStorage
+        # relationship between State and City
+
         @property
         def cities(self):
             """Get a list of all related City objects."""
+            from models import storage
+
             city_list = []
-            for city in list(models.storage.all(City).values()):
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
