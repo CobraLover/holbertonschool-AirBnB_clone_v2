0455487a8d8662d12a5a575119c96fadafffbdb1
CobraLover <6716@holbertonstudents.com>
2024-04-12 17:21:22 +0200

    Task.0-10

--- models/state.py
+++ models/state.py
@@ -3,8 +3,6 @@
 import models
 from models.base_model import BaseModel, Base
 from models.city import City
-from os import getenv
-import sqlalchemy
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -14,7 +12,7 @@ class State(BaseModel, Base):
     if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state")
+        cities = relationship("City", cascade="all, delete", backref="states")
     else:
         name = ""
 
