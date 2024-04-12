91e63b1db457a4d330a6f008a4f05e91c94c9044
CobraLover <6716@holbertonstudents.com>
2024-04-12 11:00:46 +0200

    Task.0-10

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,47 +4,57 @@ Script that starts a Flask web application:
 listening on 0.0.0.0, port 5000
 with Fifth routes
 """
+
 from flask import Flask, escape, request
+
 from flask import render_template
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """ First Route that display Hello HBNB"""
+def hello_hbnb():
+    """ display Hello HBNB! """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hello_hbnb():
-    """ Second Route that display HBNB"""
+def hbnb():
+    """ display HBNB """
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def hello_text(text):
-    """ Third Route that display C and text"""
-    return "C {}".format(text.replace("_", " "))
+def c_text(text):
+    """ display “C ”, followed by the value of the <text> 
+
+    replace underscore _ symbols with a space """
+    return "C {}".format(text.replace('_', ' '))
 
 
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def hello_python(text):
-    """ Fourth Route that display Python and text """
-    return "Python {}".format(text.replace("_", " "))
+def python_text(text):
+    """ display “Python ”, followed by the value of the <text> 
+
+    replace underscore _ symbols with a space """
+    return "Python {}".format(text.replace('_', ' '))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def hello_number(n):
-    """ Fifth Route that display Number """
-    return "%d is a number" % n
+def number(n):
+    """ display “n is a number” only if n is an integer """
+    return "{:d} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def hello_html(n):
-    """ Route that display HTML """
+def html(n):
+    """ display a HTML page only if n is an integer: 
+
+    H1 tag: “Number: n” inside the tag BODY """
     return render_template('5-number.html', n=n)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
